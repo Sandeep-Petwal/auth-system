d754b59729de971913da8c9226123eb808720cb2
Sandeep Prasad <98460341+Sandeep-Petwal@users.noreply.github.com>
2024-12-21 09:53:04 +0530

    Update cron.js

--- util/cron.js
+++ util/cron.js
@@ -2,9 +2,9 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 const pingServer = async () => {
-    cron.schedule('*/4  * * * *', async () => {
+    cron.schedule('*/9 * * * *', async () => {
         try {
-            const HOST_URL = HOST_URL;
+            const HOST_URL = process.env.HOST_URL;
             const response = await axios.get(HOST_URL);
             if (response.status === 200) {
                 console.log('Home route pinged successfully.');
