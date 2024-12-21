76546cd9719620a60e4e32dde147aeac021b6954
Sandeep Prasad <98460341+Sandeep-Petwal@users.noreply.github.com>
2024-12-21 10:34:18 +0530

    Update rateLimit.js

--- util/rateLimit.js
+++ util/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit")
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 15 minutes
-	limit: 100,
+	limit: 200,
 	standardHeaders: 'draft-8', 
 	legacyHeaders: false, 
 })
